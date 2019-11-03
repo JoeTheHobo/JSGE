939edf2c488efa3277868ae23cd8060e8e311881
JoeTheHobo <36902814+JoeTheHobo@users.noreply.github.com>
2019-11-02 20:44:44 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+startEngine(function() {
+    
+});
+
---
+++ options.js
@@ -0,0 +1,15 @@
+var game_space = $('game'); //Where to do game
+
+//Backgrounds/Sprites
+var fileEndName = 'png';
+var gameBackgrounds = [];
+var setBackground = 1;
+
+var addSprites = [];
+
+//DONT TOUCH
+window.sprites = [];
+var newSprites = [];
+function newSprite(obj) {
+    newSprites.push(obj);
+}
